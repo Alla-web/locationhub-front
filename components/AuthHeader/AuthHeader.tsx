773683a32d6f7f90d01655dad4f0b9146144f695
Roman-Shliapin <romashlaypin@gmail.com>
2026-03-30 22:54:24 +0200

    update styles

--- components/AuthHeader/AuthHeader.tsx
+++ components/AuthHeader/AuthHeader.tsx
@@ -5,7 +5,9 @@ const AuthHeader = () => {
   return (
     <header className={css.authHeader}>
       <Link href="/" className={css.logo}>
-        <span className={css.icon} />
+        <svg className={css.icon} aria-hidden="true">
+          <use href="/icons.svg#icon-map_search" />
+        </svg>
         <span className={css.authHeaderText}>Relax Map</span>
       </Link>
     </header>
