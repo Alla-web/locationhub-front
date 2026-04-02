f243676d08f88f7721dab69617119d43aeed7c41
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-02 13:46:50 +0200

    location search box UI finished

--- components/locationSearchBox/locationSearchBox.tsx
+++ components/locationSearchBox/locationSearchBox.tsx
@@ -43,53 +43,74 @@ export default function LocationSearchBox({
         />
 
         <div className={css.searchRegionContainer}>
-          <select
-            className={`${css.inputSelects} ${css.regionTypesSelects} ${css.selects}`}
-            name="region"
-            value={filters.regionId}
-            onChange={handleSelectChange}
+          <div
+            className={`${css.selectWrapper} ${css.regionLocationtypesSelects}`}
           >
-            <option value="">Регіон</option>
-            {regions.map((region) => (
-              <option key={region._id} value={region._id}>
-                {region.name}
-              </option>
-            ))}
-          </select>
+            <select
+              className={`${css.inputSelects} ${css.selects}`}
+              name="region"
+              value={filters.regionId}
+              onChange={handleSelectChange}
+            >
+              <option value="">Регіон</option>
+              {regions.map((region) => (
+                <option key={region._id} value={region._id}>
+                  {region.name}
+                </option>
+              ))}
+            </select>
 
-          <select
-            className={`${css.inputSelects} ${css.regionTypesSelects} ${css.selects}`}
-            name="locationType"
-            value={filters.locationTypeId}
-            onChange={handleSelectChange}
+            <svg className={css.selectIcon} aria-hidden="true">
+              <use href="/icons.svg#icon-keyboard_arrow_down" />
+            </svg>
+          </div>
+
+          <div
+            className={`${css.selectWrapper} ${css.regionLocationtypesSelects}`}
           >
-            <option value="">Тип локації</option>
-            {locationTypes.map((type) => (
-              <option key={type._id} value={type._id}>
-                {type.name}
-              </option>
-            ))}
-          </select>
+            <select
+              className={`${css.inputSelects} ${css.selects}`}
+              name="locationType"
+              value={filters.locationTypeId}
+              onChange={handleSelectChange}
+            >
+              <option value="">Тип локації</option>
+              {locationTypes.map((type) => (
+                <option key={type._id} value={type._id}>
+                  {type.name}
+                </option>
+              ))}
+            </select>
+
+            <svg className={css.selectIcon} aria-hidden="true">
+              <use href="/icons.svg#icon-keyboard_arrow_down" />
+            </svg>
+          </div>
         </div>
       </div>
 
-      <select
-        className={`${css.inputSelects} ${css.selects}`}
-        name="sort"
-        value={filters.sort}
-        onChange={handleSelectChange}
-      >
-        <option value="">Сортування</option>
+      <div className={css.selectWrapper}>
+        <select
+          className={`${css.inputSelects} ${css.selects}`}
+          name="sort"
+          value={filters.sort}
+          onChange={handleSelectChange}
+        >
+          <option value="">Сортування</option>
 
-        <option value="name-asc">Назва A → Я</option>
-        <option value="name-desc">Назва Я → А</option>
+          <option value="name-asc">Назва A → Я</option>
+          <option value="name-desc">Назва Я → А</option>
 
-        <option value="rate-asс">Рейтинг ↑</option>
-        <option value="rate-desc">Рейтинг ↑</option>
+          <option value="rate-asс">Рейтинг ↑</option>
+          <option value="rate-desc">Рейтинг ↑</option>
 
-        <option value="newest">Спочатку нові</option>
-        <option value="oldest">Спочатку старі</option>
-      </select>
+          <option value="newest">Спочатку нові</option>
+          <option value="oldest">Спочатку старі</option>
+        </select>
+        <svg className={css.selectIcon} aria-hidden="true">
+          <use href="/icons.svg#icon-keyboard_arrow_down" />
+        </svg>
+      </div>
     </div>
   );
 }
