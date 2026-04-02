5eeba8c211dd17197e0bd56e9eedd586306a0adb
Helena Mitiaieva <h0636641547@gmail.com>
2026-04-02 11:24:28 +0200

    fixed Header and AuthNav

--- app/(auth routes)/register/page.tsx
+++ app/(auth routes)/register/page.tsx
@@ -3,10 +3,12 @@ import { Suspense } from "react";
 import RegistrationForm from "@/components/RegistrationForm/RegistrationForm";
 
 import css from "./page.module.css";
+import AuthNav from "@/components/AuthNav/AuthNav";
 
 export default function RegisterPage() {
   return (
     <>
+      <AuthNav />
       <h1 className={css.loginTitle}>Реєстрація</h1>
       <Suspense fallback={null}>
         <RegistrationForm />
