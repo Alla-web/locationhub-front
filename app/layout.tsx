6570094c2f39858fa057d905c013e80917945048
Bohdan <bozheikob@gmail.com>
2026-04-04 12:22:28 +0000

    added right route for modal window

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,10 @@ const montserrat = Montserrat({
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="uk">
@@ -44,7 +46,7 @@ export default function RootLayout({
         <TanStackProvider>
           <AuthProvider>
             {children}
-            {/* {modal} */}
+            {modal}
           </AuthProvider>
         </TanStackProvider>
       </body>
