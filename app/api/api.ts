29f2b77288bd1cf8290e782df732ea4e317f899a
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-06 07:46:32 +0200

    /api added

--- app/api/api.ts
+++ app/api/api.ts
@@ -2,7 +2,9 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 
+// тут шде звернення на БЕКЕНД,
+// тому всі URL у маршрутах мають точно співпадати з ендпоінтами на бекенді
 export const api = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: `${process.env.BACKEND_URL}/api`,
   withCredentials: true,
 });
