121cbb9d3e368e8fe980e42e706e93938e2e72dc
Maksym Baziuk <m.bazyk@gmail.com>
2026-04-06 22:47:28 +0300

    Update page.tsx

--- app/@modal/(.)edit/page.tsx
+++ app/@modal/(.)edit/page.tsx
@@ -1,6 +1,5 @@
 import EditProfileModal from "@/components/EditProfileModal/EditProfileModal";
 
 export default function ProfileUpdateModalPage() {
-  // Просто рендеримо модалку, вона сама знає, як себе закрити!
   return <EditProfileModal />;
 }
