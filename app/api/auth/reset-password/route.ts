26867e86bc8c43742744b12ebab741eb17059b90
Alla Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-06 07:52:13 +0200

    Fix API endpoint for password reset

--- app/api/auth/reset-password/route.ts
+++ app/api/auth/reset-password/route.ts
@@ -8,7 +8,7 @@ export async function POST(req: NextRequest) {
     const token = req.nextUrl.searchParams.get("token");
 
     const apiRes = await api.post(
-      `/api/auth/reset-password?token=${token}`,
+      `/auth/reset-password?token=${token}`,
       body,
     );
 
