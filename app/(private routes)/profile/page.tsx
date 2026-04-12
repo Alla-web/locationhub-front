95d75b41c2ec11ea1dd179d29651c90f7a616b66
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-12 09:14:15 +0200

    rofile page with fully background jn 1440px

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,15 +1,19 @@
 import ProfileInfo from "@/components/ProfileInfo/ProfileInfo";
 import ProfileLocationsGrid from "@/components/ProfileLocationsGrid/ProfileLocationsGrid";
-import css from "./ProfilePage.module.css";
+import css from "./page.module.css";
 
 export default function PrivateProfilePage() {
   return (
-    <main className={css.profilePage}>
-      <ProfileInfo isPrivate={true} />
+    <div className={css.pageWrapper}>
+      <main className={css.profilePage}>
+        <div className="container">
+          <ProfileInfo isPrivate={true} />
 
-      <div className={css.contentSection}>
-        <ProfileLocationsGrid isPrivate={true} />
-      </div>
-    </main>
+          <div className={css.contentSection}>
+            <ProfileLocationsGrid isPrivate={true} />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
