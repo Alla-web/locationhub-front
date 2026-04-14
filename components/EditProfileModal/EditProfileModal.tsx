3217f207154c4877c109cd9c15c56c1c0772f4d7
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-14 11:41:20 +0200

    useWebWorker: false

--- components/EditProfileModal/EditProfileModal.tsx
+++ components/EditProfileModal/EditProfileModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState, MouseEvent } from "react";
+import { useRef, useState, MouseEvent } from "react";
 import { useRouter } from "next/navigation";
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
 import * as Yup from "yup";
@@ -72,11 +72,15 @@ export default function EditProfileModal() {
     }
 
     try {
+      if (previewUrl.startsWith("blob:")) {
+        URL.revokeObjectURL(previewUrl);
+      }
+
       if (file.size > 1 * 1024 * 1024) {
         const compressedBlob = await imageCompression(file, {
           maxSizeMB: 1,
           maxWidthOrHeight: 1920,
-          useWebWorker: true,
+          useWebWorker: false,
         });
 
         const finalType = compressedBlob.type || file.type;
@@ -111,9 +115,6 @@ export default function EditProfileModal() {
         // if (previewUrl && previewUrl !== user?.avatarUrl) {
         //   URL.revokeObjectURL(previewUrl);
         // }
-        if (previewUrl.startsWith("blob:")) {
-          URL.revokeObjectURL(previewUrl);
-        }
 
         setSelectedFile(compressedFile);
         setPreviewUrl(URL.createObjectURL(compressedFile));
@@ -123,9 +124,6 @@ export default function EditProfileModal() {
         // if (previewUrl && previewUrl !== user?.avatarUrl) {
         //   URL.revokeObjectURL(previewUrl);
         // }
-        if (previewUrl.startsWith("blob:")) {
-          URL.revokeObjectURL(previewUrl);
-        }
 
         setSelectedFile(file);
         setPreviewUrl(URL.createObjectURL(file));
